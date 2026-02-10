2c42e5d7e736391798b152e428a018089e80ea59
Brandon Harrington <brandon.harrington.abq@gmail.com>
2026-02-10 11:48:16 -0600

    Simplifies lighting calculation in the fragment shader.

--- BlockParty/cube-frag.glsl
+++ BlockParty/cube-frag.glsl
@@ -19,18 +19,18 @@ void main()
 	vec3 viewDir  = normalize(viewPos - fragPos);
 	vec3 reflDir  = reflect(-lightDir, aNormal);
 
-	vec3 ambient = objColor * lightColor * intensity * 0.1f;
+	float ambient = intensity * 0.1f;
 
 
 	float normCos = max(dot(aNormal, normalize(lightPos - fragPos)), 0.0f);
-	vec3  diffuse = normCos * lightColor * objColor * intensity;
+	float diffuse = normCos * intensity;
 
 
 	float strength = pow(max(dot(viewDir, reflDir), 0.0), 10);
-	vec3  specular = 0.5f * strength * lightColor;
+	float specular = 0.5f * strength;
 
 
-	vec3 lighting = min(ambient + diffuse + specular, 1.0f);
+	vec3 lighting = min((ambient + diffuse + specular) * objColor * lightColor, 1.0f);
 
 	FragColor = vec4(lighting, 1.0f);
 }
